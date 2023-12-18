33953e4fe168f6c20474b4dede095918b340cd1c
Khagou <97949265+Khagou@users.noreply.github.com>
2023-12-18 18:02:35 +0100

    Delete env_base/artifact_registry_repo directory

--- env_base/artifact_registry_repo/variables.tf
+++
@@ -1,8 +0,0 @@
-# artifact_registry_repo / variables.tf
-
-variable "gcp_region" {
-  description = "The GCP region to deploy the runner into."
-}
-variable "docker-repo" {
-  description = "Repo name."
-}
